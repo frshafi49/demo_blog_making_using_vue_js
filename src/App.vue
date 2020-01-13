0d138ff6bb2ca932ec7fd52e42afd252db64d0a0
shafi <frshafi49@gmail.com>
2020-01-13 14:52:55 +0600

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <add-blog/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AddBlog from './components/AddBlog.vue'
+
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    AddBlog
   }
 }
 </script>
@@ -19,10 +19,6 @@ export default {
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
